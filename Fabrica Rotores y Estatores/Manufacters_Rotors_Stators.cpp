ffa04806ef8ebf3ea974b81c7b272bbc4a7f3a40
Diego Sequeira <jsequeirag@ucenfotec.ac.cr>
2024-02-17 16:24:12 -0600

    Commit count boxes method - WIP

--- Fabrica Rotores y Estatores/Manufacters_Rotors_Stators.cpp
+++ Fabrica Rotores y Estatores/Manufacters_Rotors_Stators.cpp
@@ -13,6 +13,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include "Cook_Tree.h"
+#include "Impregnate_Tree.h"
 
 int main()
 {
@@ -20,7 +22,48 @@ int main()
     return 0;
 }
 
+bool endsWithZeroOrFive(int n) {
+    return (n % 10 == 0) || (n % 10 == 5);
+}
+
 //--Las piezas se empacan en cajas de 5 unds.--
+void CountBoxes(Cook_Node *cook, Impregnate_Node *impreg) {
+    /*Declared Variables*/
+    int cook_pieces = cook->cook.length,
+    impregnate_pieces = impreg->impreg.length,
+    boxes = 0;
+    float total_pieces = 0.0,
+        single_pieces = 0.0;
+
+    /*Counted units for each box*/
+    total_pieces = cook_pieces + impregnate_pieces;
+    boxes = (cook_pieces + impregnate_pieces) / 5;
+
+    /*Conditioning for printing boxes and single units*/
+    if (boxes != 0 && endsWithZeroOrFive(boxes) == true){
+        cout << " " << endl;
+        cout << "There is a total of ";
+        cout << boxes;
+        cout << "boxes" << endl;
+        cout << " " << endl;
+        cout << "No units without package" << endl;
+        cout << " " << endl;
+    }
+    else {
+        boxes = boxes * 5;
+        single_pieces = total_pieces - boxes;
+
+        cout << " " << endl;
+        cout << "There is a total of ";
+        cout << boxes;
+        cout << "boxes" << endl;
+        cout << " " << endl;
+        cout << "There are";
+        cout << single_pieces << endl;
+        cout << "units without package" << endl;
+        cout << " " << endl;
+    }
+};
 
 void menu() {
 
